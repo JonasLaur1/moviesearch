d8c930cfd583b420c73ef2902f7de4797d951b8c
Jonas Laurinavičius <jonaslaurinavicius1@gmail.com>
2024-07-10 02:18:53 +0300

    Fixed issues

---
+++ movieapp/src/App.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import MovieList from './components/MovieList';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
+import RemoveFavorites from './components/RemoveFavorites';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [favorites, setFavorites] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  }
+
+  const addFavoriteMovie = (movie) => {
+    const newFavoriteList = [...favorites, movie];
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+
+  const removeFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorites) => favorites.imdbID !== movie.imdbID
+    );
+    setFavorites(newFavoriteList);
+    saveToLocalStorage(newFavoriteList);
+  }
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  useEffect(() => {
+    const movieFavorites = JSON.parse(
+      localStorage.getItem('react-movie-app-favorites')
+    ) || [];
+    setFavorites(movieFavorites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favorites', JSON.stringify(items));
+  }
+
+  return(
+    <div className='container-fluid movie-app'>
+
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+
+      <div className='row'>
+        <MovieList movies={movies} addFavoriteMovie={addFavoriteMovie} favoriteComponent={AddFavorites}/>
+      </div>
+
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Favorites' />
+      </div>
+      <div className='row'>
+      <MovieList movies={favorites} addFavoriteMovie={removeFavoriteMovie} favoriteComponent={RemoveFavorites}/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App;
