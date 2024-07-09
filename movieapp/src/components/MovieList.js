abe1f5232c9261e85a2f6603b9cf4e66c10cdeeb
Jonas Laurinavičius <jonaslaurinavicius1@gmail.com>
2024-07-10 01:40:44 +0300

    Fully finished app
    Implemented all the features

--- movieapp/src/components/MovieList.js
+++ movieapp/src/components/MovieList.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const MovieList = ({ movies }) => {
+const MovieList = ( props ) => {
+    const FavoriteComponent = props.favoriteComponent;
     return(
-        <>
-            {movies.map((movie, index) => 
-            <div className="d-flex justify-content-start n-3">
-                <img src={movie.Poster} alt={movie.Title} />
-            </div>)}
-        </>
+        <div className="d-flex flex-row flex-nowrap overflow-auto">
+            {props.movies.map((movie, index) => 
+			    <div className='image-container d-flex justify-content-start m-4'>
+                    <img src={movie.Poster} alt={movie.Title} />
+                    <div onClick={() => props.addFavoriteMovie(movie)} className="overlay d-flex align-items-center justify-content-center">
+                        <FavoriteComponent/>
+                    </div>
+                </div>
+                )}
+        </div>
     )
 }
 
